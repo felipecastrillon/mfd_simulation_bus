420222e165ded52838961970c8a37e2b00a674b2
Felipe <felipecastrillon87@gmail.com>
2015-06-28 16:59:12 -0400

    removed some files

--- CityCA_4way_beta/build/classes/myClasses/mySB.rs
+++
@@ -1,4 +0,0 @@
-ca.mySB
-ca.mySB$3
-ca.mySB$2
-ca.mySB$1
--- CityCA_4way_beta/src/models/Simulator.java
+++
@@ -1,7 +0,0 @@
-package models;
-
-public interface Simulator {
-  void init(int MAXSPEED[], int LENGTH, double deltaX);
-  int updateSpeed(int speed, int gap, double prob_slowdown, double lambda,int type);
-  String getName();
-}
